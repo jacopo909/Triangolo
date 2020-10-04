f57bd6a657d92d62c6d2326af26292d8d74ea70f
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2020-10-04 16:59:53 +0200

    Add files via upload

---
+++ Program,.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nQuanto vale il 1° lato? ");
+            double lato_1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nQuanto vale il 2° lato? ");
+            double lato_2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nQuanto vale la bese? ");
+            double base_ = double.Parse(Console.ReadLine());
+            Triangolo calcola = new Triangolo(lato_1,lato_2,base_);
+
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ Triangolo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangolo
+{
+    class Triangolo
+    {
+        private double x, y, z;
+
+        public Triangolo(double Lato_1, double Lato_2, double base_)
+        {
+            x = Lato_1;
+            y = Lato_2;
+            z = base_;
+
+            if (Lato_1 == Lato_2 && Lato_2 == base_ && base_ == Lato_1 )
+            {
+                Equilatero();
+            }
+            else if (Lato_1 == Lato_2 || Lato_2 == Lato_1)
+            {
+                Isoscele();
+            }
+            else
+            {
+                Scaleno();
+            }
+        }
+        private double Perimetro()
+        {
+            double perimetro = x + y + z;
+            return perimetro;
+        }
+        
+        private void Isoscele()
+        {
+             
+            Console.WriteLine($"\nIl perimetro è {Perimetro()} e il triangolo è isoscele");
+        }
+        private void Scaleno()
+        {
+            Console.WriteLine($"\nIl perimetro è {Perimetro()} e il triangolo è scaleno");
+        }
+        private void Equilatero()
+        {
+            Console.WriteLine($"\nIl perimetro è {Perimetro()} e il triangolo è equilatero");
+        }
+    }
+}
